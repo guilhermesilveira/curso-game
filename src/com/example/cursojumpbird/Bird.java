69ddae63744acacd6643f6b566c31b3d6a28c7fc
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2014-05-08 17:25:47 -0300

    final, nome de variaveis, criacao de cor em Cores etc

--- src/com/example/cursojumpbird/Bird.java
+++ src/com/example/cursojumpbird/Bird.java
@@ -5,20 +5,16 @@ import android.graphics.Paint;
 
 public class Bird {
 
-	private int alturaDaBolinha;
-	private Paint paint = new Paint();
+	private int alturaDaBolinha = 100;
 	private ScreenHelper helper;
-	private int raioDaBolinha;
+	private int raioDaBolinha = 50;
 	
 	public Bird(ScreenHelper helper) {
 		this.helper = helper;
-		paint.setColor(0xFFFF0000);
-		alturaDaBolinha = 100;
-		raioDaBolinha = 50;
 	}
 	
 	public void drawOn(Canvas canvas) {
-		canvas.drawCircle(100, alturaDaBolinha, raioDaBolinha, paint);
+		canvas.drawCircle(100, alturaDaBolinha, raioDaBolinha, Cores.azulChoque());
 	}
 	
 	public void cai() {
